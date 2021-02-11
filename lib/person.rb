d1314dd5e2704dcd84cd0b17ac240434d8cdcc72
Rene <renegarcia1864@gmail.com>
2021-02-11 00:05:31 -0600

    commit tree e3e7e043c144ca6214572b37f23423296f0df767

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,79 @@
 # your code goes here
+require 'pry'
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(num)
+        @happiness = num
+        @happiness = 10 if @happiness > 10
+        @happiness = 0 if @happiness < 0
+    end
+
+    def hygiene=(num)
+        @hygiene = num
+        @hygiene = 10 if @hygiene > 10
+        @hygiene = 0 if @hygiene < 0
+    end
+
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(salary)
+        @bank_account = @bank_account += salary
+        @bank_account
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        objects = [self, person] #two people are having the conversation
+        if topic == 'politics'
+            objects.each { |person| person.happiness -= 2}
+            'blah blah partisan blah lobbyist'
+        elsif topic == 'weather'
+            objects.each { |person| person.happiness += 1}
+            'blah blah sun blah rain'
+        else topic == 'other'
+            objects.each {|person| person.happiness += 0}
+            "blah blah blah blah blah"
+        end
+    end
+
+end 
